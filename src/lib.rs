dc8539bb0b241bc2c009698617d95ec4505613cb
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2026-01-21 02:34:56 -0400

    chore: init base project

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+use freya::prelude::*;
+
+mod app;
+mod components;
+mod dialog;
+mod utils;
+
+use app::init;
+
+pub const APP_NAME: &str = env!("CARGO_CRATE_NAME");
+
+#[cfg(target_os = "android")]
+use winit::platform::android::activity::AndroidApp;
+
+#[cfg(target_os = "android")]
+#[no_mangle]
+fn android_main(droid_app: AndroidApp) {
+    use winit::platform::android::EventLoopBuilderExtAndroid;
+
+    launch(
+        LaunchConfig::new().with_window(
+            WindowConfig::new(init)
+                .with_size(500., 450.)
+                .with_window_attributes(|_attr, event_loop_builder| {
+                    event_loop_builder.with_android_app(droid_app)
+                }),
+        ),
+    )
+}
+
+#[allow(dead_code)]
+#[cfg(not(target_os = "android"))]
+fn main() {
+    use tracing::level_filters::LevelFilter;
+    use tracing_subscriber::EnvFilter;
+
+    use self::utils::data_dir;
+
+    let builder = tracing_appender::rolling::Builder::new()
+        .rotation(tracing_appender::rolling::Rotation::DAILY)
+        .filename_suffix("log")
+        .build(data_dir(&["logs"]))
+        .unwrap();
+    let (non_blocking, _guard) = tracing_appender::non_blocking(builder);
+    tracing_subscriber::fmt()
+        .with_ansi(false)
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::TRACE.into())
+                .try_from_env()
+                .unwrap_or_default(),
+        )
+        .with_writer(non_blocking)
+        .init();
+
+    launch(LaunchConfig::new().with_window(WindowConfig::new(init).with_size(600., 450.)))
+}
