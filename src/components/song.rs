9ecdd9fd80d834e2588f647c356126b76e756f9e
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2026-01-28 16:01:20 -0400

    feat: song component complete

--- src/components/song.rs
+++ src/components/song.rs
@@ -1,6 +1,7 @@
 use std::ops::Not;
 use std::str::FromStr;
 
+use freya::animation::*;
 use freya::icons::lucide::{audio_lines, play};
 use freya::prelude::*;
 
@@ -16,12 +17,12 @@ pub struct SongInfo {
 impl Default for SongInfo {
     fn default() -> Self {
         Self {
-    title: "Amor Como Fuego".into(),
-    ty: "Canción".into(),
-    details: "Hillsong En Español".into(),
-    is_album: false,
-    is_artist: false,
-    thumbnail: "https://lh3.googleusercontent.com/8daRI8WzbLKSrodjKXQy-50Yegbuxmh16mF7BQ8aTxFtku67M7Wh4oaWNzy38uNL_blAd8FW18drn4OmxQ=w226-h226-l90-rj".into(),
+            title: "Amor Como Fuego".into(),
+            ty: "Canción".into(),
+            details: "Hillsong En Español".into(),
+            is_album: true,
+            is_artist: false,
+            thumbnail: "https://lh3.googleusercontent.com/8daRI8WzbLKSrodjKXQy-50Yegbuxmh16mF7BQ8aTxFtku67M7Wh4oaWNzy38uNL_blAd8FW18drn4OmxQ=w226-h226-l90-rj".into(),
         }
     }
 }
@@ -31,7 +32,22 @@ pub fn song(info: SongInfo) -> impl IntoElement {
     let mut size = use_state(|| 223.);
     let mut play_btn_size = use_state(|| 48.);
     let mut hover = use_state(|| false);
-    let mut hover_options = use_state(|| false);
+
+    let mut anim_album_play = use_animation(|_| {
+        (
+            AnimColor::new(Color::BLACK.with_a(180), Color::BLACK)
+                .function(Function::Sine)
+                .ease(Ease::InOut)
+                .time(100),
+            AnimNum::new(40., 60.)
+                .function(Function::Sine)
+                .ease(Ease::InOut)
+                .time(100),
+        )
+    });
+
+    let (album_play_color, album_play_size) = anim_album_play.read().value();
+    let album_play_size = Size::px(album_play_size);
 
     rect()
         .vertical()
@@ -42,12 +58,8 @@ pub fn song(info: SongInfo) -> impl IntoElement {
                 .center()
                 .width(Size::px(size()))
                 .height(Size::px(size()))
-                .rounded_lg()
+                .corner_radius(if info.is_artist { size() } else { 8. })
                 .overflow(Overflow::Clip)
-                .on_press(move |_| {
-                    println!("toggle");
-                    is_playing.toggle()
-                })
                 .on_pointer_enter(move |_| {
                     Cursor::set(CursorIcon::Pointer);
                     hover.set(true);
@@ -62,45 +74,67 @@ pub fn song(info: SongInfo) -> impl IntoElement {
                         .expanded()
                         .center()
                         .image_cover(ImageCover::Center)
-                        // center play button
-                        .maybe_child(
-                            (!info.is_album || !info.is_artist).then_some(
-                                svg(if *is_playing.read() { audio_lines() } else { play() })
-                                    .fill(Color::WHITE)
-                                    .color(Color::WHITE)
-                                    .width(Size::px(play_btn_size()))
-                                    .height(Size::px(play_btn_size()))
-                                    .position(
-                                        Position::new_absolute()
-                                            .top((size() / 2.) - (play_btn_size() / 2.))
-                                            .left((size() / 2.) - (play_btn_size() / 2.)),
-                                    ),
-                            ),
-                        )
-                        // context right top
-                        .maybe_child(
-                            (*hover.read() && !info.is_artist).then_some(
-                                rect()
-                                    .width(Size::px(36.))
-                                    .height(Size::px(36.))
-                                    .rounded_full()
-                                    .on_pointer_enter(move |_| hover_options.set(true))
-                                    .on_pointer_leave(move |_| hover_options.set(false))
-                                    .position(Position::new_absolute().top(8.).right(8.))
-                                    .background(Color::WHITE.with_a(if *hover_options.read() {
-                                        180
+                        .center()
+                        .child(
+                            rect()
+                                .expanded()
+                                .center()
+                                .on_press(move |_| {
+                                    if !info.is_album && !info.is_artist {
+                                        is_playing.toggle()
+                                    }
+                                })
+                                .background(Color::BLACK.with_a(
+                                    if *hover.read() && !info.is_artist {
+                                        60
                                     } else {
                                         0
-                                    })),
-                            ),
+                                    },
+                                ))
+                                // center play button
+                                .maybe_child(
+                                    (!info.is_album && !info.is_artist).then_some(
+                                        svg(if *is_playing.read() {
+                                            audio_lines()
+                                        } else {
+                                            play()
+                                        })
+                                        .fill(Color::WHITE)
+                                        .color(Color::WHITE)
+                                        .width(Size::px(play_btn_size()))
+                                        .height(Size::px(play_btn_size())),
+                                    ),
+                                ),
                         )
-                        .child(rect().expanded().background(Color::BLACK.with_a(
-                            if *hover.read() && !info.is_artist {
-                                60
-                            } else {
-                                0
-                            },
-                        ))),
+                        // album play/pause right bottom
+                        .maybe_child(
+                            (*hover.read() && info.is_album).then_some(
+                                rect()
+                                    .width(album_play_size.clone())
+                                    .height(album_play_size)
+                                    .rounded_full()
+                                    .padding(12.)
+                                    .on_press(move |_| is_playing.toggle())
+                                    .on_pointer_enter(move |_| {
+                                        anim_album_play.start();
+                                    })
+                                    .on_pointer_leave(move |_| {
+                                        anim_album_play.reverse();
+                                    })
+                                    .position(Position::new_absolute().bottom(8.).right(8.))
+                                    .background(album_play_color)
+                                    .child(
+                                        svg(if *is_playing.read() {
+                                            audio_lines()
+                                        } else {
+                                            play()
+                                        })
+                                        .fill(Color::WHITE)
+                                        .color(Color::WHITE)
+                                        .expanded(),
+                                    ),
+                            ),
+                        ),
                 ),
         )
         .child(
