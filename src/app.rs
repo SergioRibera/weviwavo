95b9797178b1957847d05fa45defd12d2b6da817
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2026-02-01 17:25:31 -0400

    refactor: move app to AppComponent

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,48 @@
-use freya::prelude::*;
-use freya_radio::prelude::*;
+use freya::{prelude::*, radio::*};
 
-use crate::components::song;
+use crate::components::Section;
 
-pub fn init() -> impl IntoElement {
-    use_init_root_theme(|| PreferredTheme::Dark.to_theme());
-    // use_init_radio_station::<Data, DataChannel>(Data::default);
-    // let mut radio = use_radio::<Data, DataChannel>(DataChannel::ListCreation);
+mod data;
 
-    // let on_press = move |_| {
-    //     radio.write().lists.push(Vec::default());
-    // };
+pub use data::*;
 
-    rect()
-        .center()
-        .vertical()
-        .expanded()
-        .theme_color()
-        .theme_background()
-        .child(song(Default::default()))
+pub struct MainApp {
+    pub radio: RadioStation<Data, DataChannel>,
+}
+
+impl Into<AppComponent> for MainApp {
+    fn into(self) -> AppComponent {
+        AppComponent::new(self)
+    }
+}
+
+impl Component for MainApp {
+    fn render(&self) -> impl IntoElement {
+        use_share_radio(move || self.radio);
+        use_init_root_theme(|| PreferredTheme::Dark.to_theme());
+        let radio = use_radio::<Data, DataChannel>(DataChannel::Feed);
+
+        // let on_press = move |_| {
+        //     radio.write().lists.push(Vec::default());
+        // };
+
+        // https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3Dhttps%253A%252F%252Fmusic.youtube.com%252F%26feature%3D__FEATURE__&dsh=S-332057247%3A1769407389042975&hl=en&ifkv=AXbMIuBcVMXNf7v5tojo4FavD_6iFXGCjqj3iUhQOJwznaQ75Q4GaUS5mvFxOobbGpOODtpGwZza&ltmpl=music&passive=true&service=youtube&uilel=3&flowName=GlifWebSignIn&flowEntry=ServiceLogin
+
+        let feed = radio.read().feed.clone();
+        println!("Sections: {}", feed.len());
+
+        rect()
+            .center()
+            .vertical()
+            .expanded()
+            .theme_color()
+            .theme_background()
+            .child(
+                ScrollView::new()
+                    .expanded()
+                    .direction(Direction::Vertical)
+                    .spacing(20.)
+                    .children(feed.into_iter().map(Section).map(IntoElement::into_element)),
+            )
+    }
 }
