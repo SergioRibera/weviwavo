47a4d0ce6d721fb2f29fe336375081cb13a5bb5f
wilovy <emiliano@adquiere.co>
2026-04-08 01:30:41 -0600

    docs: Update README

--- README.md
+++ README.md
@@ -2,18 +2,4 @@
 
 ## How to test
 
-Currenttly the project contains a `.env.example`, the first step is create a `.env` file, in that file you have to copy the variable `TEST_COOKIE`, then you need to specify the cookie for visualize the components according to your feed, for get the cookie, the easiest way is:
-
-- Open a private tab in your browser
-- Go to [youtube music](https://music.youtube.com)
-- Open de developer tools (inspect an element or f12)
-- In the developer tools go to `Network` tab
-- Log in on youtube music with your google account
-- In the search bar in the `Network` tab search `browse?` 
-- Scroll down until find the cookie fild and copy the entire value in the .env variable
-
-This is an example of the cookie in Chrome Browser
-![cookie_image](./.github/images/cookie.webp)
-
-Once you maded the process for get the cookie you can test executing the app with `just` command
-
+You need to login in [youtube music](https://music.youtube.com) on Microsoft Edge, Google Chrome browser or Firefox.
--- src/app.rs
+++ src/app.rs
@@ -6,10 +6,6 @@ mod data;
 
 pub use data::*;
 
-const LIGHT_SIZE: f32 = 1024.;
-const LIGHT_SIZE_HALF: f32 = LIGHT_SIZE / 2.;
-const COLORS: &[&str] = &["#081F22", "#1B1F15", "#000000AA", "#081F22"];
-
 #[derive(Clone)]
 pub struct MainApp {
     pub radio: RadioStation<Data, DataChannel>,
@@ -21,7 +17,9 @@ impl App for MainApp {
         use_init_root_theme(|| PreferredTheme::Dark.to_theme());
         let radio = use_radio::<Data, DataChannel>(DataChannel::Feed);
         let radio = radio.read();
-
+        let bg_image = ("bg_image", include_bytes!("../resources/bg_default.webp"));
+        let platform = Platform::get().root_size;
+        //
         // let on_press = move |_| {
         //     radio.write().lists.push(Vec::default());
         // };
@@ -31,22 +29,24 @@ impl App for MainApp {
             .expanded()
             .theme_color()
             .background(Color::BLACK)
-            .child(rect().horizontal().children((0..=5).map(|i| {
-                let i = i as usize % COLORS.len();
+            .child(
                 rect()
-                    .width(Size::px(LIGHT_SIZE))
-                    .height(Size::px(LIGHT_SIZE))
+                    .child(
+                        ImageViewer::new(bg_image).child(
+                            rect().expanded().background_linear_gradient(
+                                LinearGradient::new().stops([
+                                    GradientStop::new(Color::TRANSPARENT, 0.0),
+                                    GradientStop::new(Color::BLACK, 80.0),
+                                ]),
+                            ),
+                        ),
+                    )
                     .position(
                         Position::new_global()
-                            .left(-(LIGHT_SIZE_HALF) + (i as f32 * (LIGHT_SIZE_HALF)))
-                            .top(-(LIGHT_SIZE_HALF)),
-                    )
-                    .background_radial_gradient(RadialGradient::new().stops([
-                        GradientStop::new(Color::from_hex(COLORS[i]).unwrap(), 0.),
-                        GradientStop::new(Color::from_hex(COLORS[i]).unwrap().with_a(0), 100.),
-                    ]))
-                    .into_element()
-            })))
+                            .left(0.)
+                            .top(-(platform.read().height * 0.20)),
+                    ),
+            )
             .child(
                 rect().horizontal().children(
                     radio
